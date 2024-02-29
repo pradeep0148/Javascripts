1b300562d459ad4e6f34195c4ad17de8efc4601f
Pradeep Vishwakarma <122095172+pradeep0148@users.noreply.github.com>
2024-02-29 08:20:42 +0000

    comparison logic

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2<=2);
+// console.log(2==3);
+// console.log(2!=3);
+
+
+
+// console.log("2">1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined  == 0);
+// console.log( undefined < 0);
+// console.log( undefined >= 0);  avoid this type code 
+
+console.log("2"=== 2);
+
+
