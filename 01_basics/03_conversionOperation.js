f38b49fbebafd95c05a466d5eb1460919eef9f18
Pradeep Vishwakarma <122095172+pradeep0148@users.noreply.github.com>
2024-02-29 07:23:33 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score =false
+ console.log(typeof score);
+ console.log(typeof (score));
+  let valueInNumber =Number (score)
+  console.log(typeof valueInNumber);
+  console.log(valueInNumber);
+  // "33" => 33
+  // "33asd" => NaN
+  // true => 1; false => 0
+
+
+  let isLoggedIn = 0
+  let booleanIsLoggedIn =Boolean (isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+
+
+  // 1=> true ; 0=> false 
+  // " " =>false
+  // "hitesh" = true
+let someNumber= 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
