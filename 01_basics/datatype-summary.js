5aadb3fdc82976f88c79667b49b0caa8fd70ba3a
Pradeep Vishwakarma <122095172+pradeep0148@users.noreply.github.com>
2024-02-29 08:59:52 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,30 @@
+// # primitive
+// 7 types : string ,Number, Boolean, null, undefined,symbol,BigInt
+const score =33
+const scoreValue=100.3
+
+const isLoggedIn =false 
+const outsideTemp = null
+let userEmail;
+
+const id =Symbol("122")
+const anotherId = Symbol("122")
+console.log(id === anotherId);
+
+const bigNumber = 14154653124 
+
+// Reference (non primitive)
+// Array ,Objects, functions
+
+const heros = ["shaktiman" , "naagraj", "doga"]
+let myObj ={
+    name:"rudra",
+    age: 22,
+
+}
+
+ const myFunction = function(){
+    console.log("hello world");
+
+ }
+console.log(typeof bigNumber);
