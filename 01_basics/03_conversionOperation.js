831c3f6b8a21beefe497f4575087855c4eaad8dd
Pradeep Vishwakarma <122095172+pradeep0148@users.noreply.github.com>
2024-02-29 07:58:13 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score =false
- console.log(typeof score);
- console.log(typeof (score));
+//  console.log(typeof score);
+//  console.log(typeof (score));
   let valueInNumber =Number (score)
-  console.log(typeof valueInNumber);
-  console.log(valueInNumber);
+//   console.log(typeof valueInNumber);
+//   console.log(valueInNumber);
   // "33" => 33
   // "33asd" => NaN
   // true => 1; false => 0
@@ -19,5 +19,36 @@ let score =false
   // "hitesh" = true
 let someNumber= 22
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+//***************************Operations ********************* */
+
+let value =3
+let negativeValue= -value
+// console.log(negativeValue);
+// console.log(2+2); 
+// console.log(2-3); 
+// console.log(2*3);
+// console.log(4/2);
+// console.log(2%6);
+
+
+
+let str1 ="hello"
+let str2 = " hitesh"
+let str3=str1+str2
+// console.log(str3);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2,num3
+num1=num2=num3=2+2
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
