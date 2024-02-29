a1475aaa5f991fa797ece5c0d3de1ef1a3015a0b
Pradeep Vishwakarma <122095172+pradeep0148@users.noreply.github.com>
2024-02-29 13:17:04 +0000

    Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -25,8 +25,31 @@ myArr.shift()
 
 
 // slice , splice
-console.log("A", myArr);
-const myn1 =myArr.slice(1,3)
-console.log(myn1);
-console.log("B",myArr);
+// console.log("A", myArr);
+// const myn1 =myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArr);
+
+//************************************************************ */
+
+const marvel_heros =["thor", "ironman","spiderman"]
+const dc_heros = ["rudra","rahaul","raj"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros =marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+const allNewHehros = [...marvel_heros,...dc_heros]
+console.log(allNewHehros);
+
+
+
+// const anotherarray =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array =anotherarray.flat(Infinity)
+// console.log(real_another_array);
+console.log(Array.isArray("rudra"))
+
+
 
